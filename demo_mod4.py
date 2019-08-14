2c4845faf499b2a94db374fd7692c0c1df3af7b3
sachmb <sachham3@gmail.com>
2019-08-14 17:58:18 +0700

    new files

--- demo_mod4.py
+++ demo_mod4.py
@@ -24,11 +24,42 @@ element = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "search-input")))
 	
 search_ele = driver.find_element_by_id("search-input")
-search_ele.send_keys("Wayne Rooney")
+search_ele.send_keys("Rolando Aarons")
 search_ele.send_keys(Keys.RETURN)
 
 #clicking on wayne rooney
 driver.implicitly_wait(3)
-click_wayne = driver.find_element_by_xpath("//img[@data-player='p13017']")
-click_wayne.click()
-#page_source_overview = driver.page_source
+click_wayne = driver.find_element_by_xpath("//img[@data-player='p155513']").click()
+
+page_source_overview = driver.page_source
+
+from bs4 import BeautifulSoup
+
+#loading page source info
+soup = BeautifulSoup(page_source_overview, 'lxml')
+
+#locating the titles
+title_finder = soup.find_all("span", class_="title")
+
+title_finder
+
+print(10*"-" + "These are the latest news headlines about wayne rooney" + 10*"-" + "\n")
+for title in title_finder:
+	print(title.string)
+	
+#stats button element
+time.sleep(1)
+wayne_stats = driver.find_element_by_xpath("//a[@href='stats']").click()
+
+page_source_stats = driver.page_source
+
+soup = beautifulSoup(page_source_stats, "lxml")
+
+#locating all the stats
+stat_finder = soup.find_all("span", class_="allStatContainer")
+
+print(stat_finder)
+
+print(10*"-" + "Wayne Rooney Stats" + 10*"-" + "\n")
+for title in stat_finder:
+	print(stat["data-stat"] + " - " + stat.string) 
